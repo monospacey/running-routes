554522a91379c678f7ceba84fdd4fb8235771cfb
monospacey <>
2024-08-14 23:05:20 +1000

    Feature: Add toggle for different maps on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
 <script>
   import RadiusMap from "../lib/RadiusMap.svelte";
+  import RunningMap from "../lib/RunningMap.svelte";
+
+  let radiusMapCheck = false;
 </script>
 
 <h1>Running Routes</h1>
-<RadiusMap/>
+<div>
+  <input type="checkbox" bind:checked={radiusMapCheck} />
+  Tick for the two maps
+</div>
 
+{#if radiusMapCheck}
+  <RunningMap />
+{:else}
+  <RadiusMap />
+{/if}
