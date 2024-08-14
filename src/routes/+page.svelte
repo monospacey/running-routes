f857243dc25834d0db7fd69685e0da3f3d33badf
monospacey <>
2024-08-14 23:05:20 +1000

    Feature: Add RadiusMap!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import RadiusMap from "../lib/RadiusMap.svelte";
+</script>
+
+<h1>Running Routes</h1>
+<RadiusMap/>
+
